ba81e61b30e2c89a3e5aad2b703cdec85e69e8ed
JamiaEchols1 <jechols5@my.westga.edu>
2022-02-03 14:28:31 -0500

    Added login page to shared library
    I added a login page to the shared library

--- code/TravelPlannerWebApp/Models/User.cs
+++ code/TravelPlannerWebApp/Models/User.cs
@@ -7,5 +7,9 @@ namespace TravelPlannerWebApp.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
     }
 }
