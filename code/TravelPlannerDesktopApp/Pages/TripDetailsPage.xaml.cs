a0798208c864d41a55fbe6c66dc83baccac55710
JamiaEchols1 <jechols5@my.westga.edu>
2022-02-21 20:56:24 -0500

    merge

---
+++ code/TravelPlannerDesktopApp/Pages/TripDetailsPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TravelPlannerDesktopApp.Controls;
+using TravelPlannerLibrary.DAL;
+using TravelPlannerLibrary.Models;
+
+namespace TravelPlannerDesktopApp.Pages
+{
+    /// <summary>
+    /// Interaction logic for Trips.xaml
+    /// </summary>
+    public partial class TripDetailsPage : Page
+    {
+        private TripDAL tripDAL = new TripDAL();
+        public TripDetailsPage(Trip trip)
+        {
+            InitializeComponent();
+            this.tripsListBox.ItemsSource = tripDAL.GetTrips(LoggedUser.user.Id);
+        }
+
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {
+
+            var ClickedButton = e.OriginalSource as NavButton;
+
+            NavigationService.Navigate(ClickedButton.NavUri);
+        }
+    }
+}
