16b545be42545b65bed4def2ca0816d531295eeb
bbouwman0815 <bbouwma1@my.westga.edu>
2022-02-24 22:25:53 -0500

    Added web login added login cshtml added login layout that brings you to home landing page

---
+++ code/TravelPlannerWebApp/Controllers/LoginController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelPlannerLibrary;
+using TravelPlannerLibrary.DAL;
+using TravelPlannerLibrary.Models;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class LoginController : Controller
+    {
+        private TravelPlannerDatabaseEntities db = new TravelPlannerDatabaseEntities();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Users/Create
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([Bind(Include = "Username,Password")] UserCredentials user)
+        {
+            if (ModelState.IsValid)
+            {
+                LoginDAL loginDal = new LoginDAL();
+                var loggedUser = loginDal.Login(user.Username, user.Password);
+
+                if (loggedUser != null)
+                {
+                    LoggedUser.user = new TravelPlannerLibrary.Models.User()
+                    {
+                        Username = user.Username,
+                        Password = user.Password
+                    };
+                    return View("../Home/Index");
+                }
+
+           
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Authenticate()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Authenticate([Bind(Include = "Username,Password")] UserCredentials user)
+        {
+            if (ModelState.IsValid)
+            {
+                LoginDAL loginDal = new LoginDAL();
+                var loggedUser = loginDal.Login(user.Username, user.Password);
+
+                if (loggedUser != null)
+                {
+                    LoggedUser.user = new TravelPlannerLibrary.Models.User()
+                    {
+                        Username = user.Username,
+                        Password = user.Password
+                    };
+                    return View("../Home/Index");
+                }
+
+
+            }
+
+            return RedirectToAction("Authenticate");
+        }
+    }
+}
