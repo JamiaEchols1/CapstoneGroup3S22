417d02d1c0d30ef5fe7060782673ac329d02d910
jechols5 <jechols5@my.westga.edu>
2022-04-03 11:03:42 -0400

    Merged with BB_Features_Transportation
    I merged with the BB_Faeature_Transportation branch as well as but the map that was inside the transportation info page inside a viewbox

--- code/TravelPlannerDatabase/Transportations.sql
+++ code/TravelPlannerDatabase/Transportations.sql
@@ -8,8 +8,6 @@
 	[Origin] varchar(250) null,
 	[Destination] varchar(250) null,
 	[Type] varchar(250) not null, 
-    [Origin] VARCHAR(150) NOT NULL, 
-    [Destination] VARCHAR(150) NOT NULL, 
     CONSTRAINT [FK_dbo.Transportation_dbo.Trip_Id] FOREIGN KEY ([TripId])
 		REFERENCES [dbo].[Trips] ([Id]) On delete cascade
 )
